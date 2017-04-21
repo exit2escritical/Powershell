be83fb0c5a78d18f1ede02dbadf094dd832b01b7
exit2escritical <exit2escritical@gmail.com>
2017-04-21 12:11:13 +0200

    Check ExpireDate of Certs on Windows Certs Console
    Script that checks if Certificates installed on Windows Cert Console, under \LocalMachine\My, expire on a given data
    If so, writes the certs that expire and exit 2

---
+++ check_console_certs.ps1
@@ -0,0 +1,42 @@
+Param(
+[string] $SCRIPT_NAME,
+[string] $SERVICE_Th
+)
+
+$OK_STATUS = 0
+$WARNING_STATUS = 1
+$ERROR_STATUS = 2
+$UNKNOWN_STATUS = 3
+$DEPENDENT_STATUS = 4
+
+$NSC_TMPDIR = "C:\Program Files\NSClient++\tmp\"
+$STATUSFILE = $ICINGA_TMPDIR + ${SCRIPT_NAME}
+
+$dateDeadline = (Get-Date).AddDays($SERVICE_Th)
+
+
+If (test-path -Path $STATUSFILE) {
+    Clear-Content $STATUSFILE
+}Else {
+        new-item -Path "$STATUSFILE" -ItemType file
+ }
+
+
+Get-ChildItem -Path cert:\LocalMachine\My  | ForEach-Object {
+    $name = $_.Subject
+    $dateExpireDays = $_.NotAfter
+                        If ($dateExpireDays -lt $dateDeadline){
+                                $CRIT_MSG = "CRITICAL: [$name] expires [$dateExpireDays]"
+                                Add-Content $STATUSFILE "`n$CRIT_MSG"
+                        }
+        }
+
+If ((Get-Content $STATUSFILE) -ne $Null) {
+Get-Content $STATUSFILE| foreach {Write-Output $_}
+        Write-Host "$_"
+        exit $ERROR_STATUS
+}Else {
+        Write-Host "OK"
+        exit $OK_STATUS
+        }
+
