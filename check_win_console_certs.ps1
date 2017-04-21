4afa1676baddd932d33963bd184f0afd3d27e6b6
exit2escritical <exit2escritical@gmail.com>
2017-04-21 12:59:41 +0200

    Add new if condition
    Do not alert if cert ExpirationDate is lower that Actual Date

--- check_win_console_certs.ps1
+++ check_win_console_certs.ps1
@@ -13,7 +13,7 @@ $NSC_TMPDIR = "C:\Program Files\NSClient++\tmp\"
 $STATUSFILE = $ICINGA_TMPDIR + ${SCRIPT_NAME}
 
 $dateDeadline = (Get-Date).AddDays($SERVICE_TH)
-
+$ActualDate = (Get-Date)
 
 If (test-path -Path $STATUSFILE) {
     Clear-Content $STATUSFILE
@@ -25,10 +25,12 @@ If (test-path -Path $STATUSFILE) {
 Get-ChildItem -Path cert:\LocalMachine\My  | ForEach-Object {
     $name = $_.Subject
     $dateExpireDays = $_.NotAfter
+    		If ($dateExpireDays -gt $ActualDate){
                         If ($dateExpireDays -lt $dateDeadline){
                                 $CRIT_MSG = "CRITICAL: [$name] expires [$dateExpireDays]"
                                 Add-Content $STATUSFILE "`n$CRIT_MSG"
                         }
+             }
         }
 
 If ((Get-Content $STATUSFILE) -ne $Null) {
